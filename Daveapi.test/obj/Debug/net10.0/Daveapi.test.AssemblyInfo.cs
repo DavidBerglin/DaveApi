ac0dfac4c7fd9c11fdb5788823fdb7f94e8d054c
DavidBerglin <david.berglin@gmail.com>
2026-01-20 13:03:24 +0100

    update

--- Daveapi.test/obj/Debug/net10.0/Daveapi.test.AssemblyInfo.cs
+++ Daveapi.test/obj/Debug/net10.0/Daveapi.test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DaveApi.test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+963b64aeab1c864af843d7b7d8130cd9a4c457d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c027074985d4edc215dc44b3a0b765bc5ba1d09")]
 [assembly: System.Reflection.AssemblyProductAttribute("DaveApi.test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DaveApi.test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
